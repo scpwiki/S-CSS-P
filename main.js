d1df05aefe7c49c5e3fbe358dfbb54ac88b4d01a
bluesoul <github@nags.me>
2018-06-26 15:29:47 -0600

    Improvements

--- main.js
+++ main.js
@@ -7,9 +7,8 @@ chrome.storage.local.get(['activeTheme'], function (result) {
 	document.head.appendChild(style);
 });
 
-var logocss = ""
 chrome.storage.local.get(['activeLogo'], function (result) {
-	logocss = result.activeLogo;
+	var logocss = result.activeLogo;
 	var style2 = document.createElement('link');
 	style2.rel = 'stylesheet';
 	style2.type = 'text/css';
